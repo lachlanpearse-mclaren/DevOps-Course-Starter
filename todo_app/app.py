c69cbdab38ad7a6f9bf2c64d4b616ffe6198731a
Lachlan Pearse <lachlan.pearse@mclaren.com>
2020-11-16 15:51:50 +0000

    Status update functionality added

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template, request, redirect
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items, add_item 
+from todo_app.data.session_items import get_items, add_item, save_item, get_item
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,6 +18,18 @@ def index():
 def new_item():
     new_item_title = request.form.get('new_item_title')
     items = add_item(new_item_title)
+    return redirect('/')
+
+@app.route('/toggle_status', methods=['POST'])
+def toggle_status():
+    toggle_item = get_item(request.form.get('toggle_item_id'))
+
+    if toggle_item['status'] == "Not Started":
+        toggle_item['status'] = "Completed"
+    else:
+        toggle_item['status'] = "Not Started"
+
+    item = save_item(toggle_item)
     return redirect('/')
 
 if __name__ == '__main__':
