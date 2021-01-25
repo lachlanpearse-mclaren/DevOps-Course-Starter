6ff2341032aa5d0c8ce82f3e63318b4e063fafd5
Lachlan Pearse <lachlan.pearse@mclaren.com>
2021-01-25 20:17:29 +0000

    Updated the status column to display the list the card lives on

--- todo_app/data/trello_api.py
+++ todo_app/data/trello_api.py
@@ -11,17 +11,22 @@ def get_trello_keys():
 def get_trello_board_id():
     return '600ede7caeb0c67b818ec912'
 
-def get_trello_todo_cards():
+def get_trello_list_id(list_name):
     trello_auth_key = get_trello_keys()
     trello_board_id = get_trello_board_id()
     response = requests.get(f'https://api.trello.com/1/boards/{trello_board_id}/lists?key={trello_auth_key[0]}&token={trello_auth_key[1]}')
     
-    all_cards = response.json()
+    all_lists = response.json()
 
-    for i in all_cards:
-        if i['name'] == 'To Do':
-            todo_list_id = i['id']
+    for i in all_lists:
+        if i['name'] == list_name:
+            list_id = i['id']
     
-    response = requests.get(f'https://api.trello.com/1/lists/{todo_list_id}/cards?key={trello_auth_key[0]}&token={trello_auth_key[1]}')
+    return list_id
+
+def get_trello_cards():
+    trello_auth_key = get_trello_keys()
+    trello_board_id = get_trello_board_id()
+    response = requests.get(f'https://api.trello.com/1/boards/{trello_board_id}/cards?key={trello_auth_key[0]}&token={trello_auth_key[1]}')
 
     return response.json()
