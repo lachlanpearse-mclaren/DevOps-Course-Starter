4e403ad3b09fdd00fc9346a151180a2b041f8d64
Lachlan Pearse <lachlan.pearse@mclaren.com>
2021-03-04 11:37:29 +0000

    Got the test client to work now! Thanks Hugh!!

--- todo_app/tests/test_app.py
+++ todo_app/tests/test_app.py
@@ -18,11 +18,13 @@ def client():
 @patch('requests.get')
 def test_index_page(mock_get_requests, client):
 
-    mock_get_requests.side_effect = mock_get_lists(f'https://api.trello.com/1/boards/{trello_board_id}/lists', None)
+    mock_get_requests.side_effect = mock_get_trello
     response = client.get('/')
 
-def mock_get_lists(url, params):
-    if url == f'https://api.trello.com/1/boards/{trello_board_id}/lists':
+
+def mock_get_trello(url):
+    trello_board_id = os.getenv('TRELLO_BOARD_ID')
+    if url.startswith(f'https://api.trello.com/1/boards/{trello_board_id}/lists'):
         response = Mock()
 
         json_return = [
@@ -54,6 +56,193 @@ def mock_get_lists(url, params):
                 "subscribed": False
             }
         ]
-        response.json.return_value = json.dumps(json_return)
+        response.json.return_value = json_return
+        return response
+    elif url.startswith(f'https://api.trello.com/1/boards/{trello_board_id}/cards'):
+        response = Mock()
+
+        json_return = [
+            {
+                "id": "601bdd078a423353b3ade947",
+                "checkItemStates": None,
+                "closed": False,
+                "dateLastActivity": "2021-02-11T17:06:06.953Z",
+                "desc": "Having coffee to keep myself from falling asleep at the wheel",
+                "descData": None,
+                "dueReminder": -1,
+                "idBoard": "abcd1234",
+                "idList": "600ede7caeb0c67b818ec913",
+                "idMembersVoted": [],
+                "idShort": 22,
+                "idAttachmentCover": None,
+                "idLabels": [],
+                "manualCoverAttachment": False,
+                "name": "Coffee",
+                "pos": 16384,
+                "shortLink": "JlDCgB0e",
+                "isTemplate": False,
+                "cardRole": None,
+                "badges": {
+                    "attachmentsByType": {
+                        "trello": {
+                            "board": 0,
+                            "card": 0
+                        }
+                    },
+                    "location": False,
+                    "votes": 0,
+                    "viewingMemberVoted": False,
+                    "subscribed": False,
+                    "fogbugz": "",
+                    "checkItems": 0,
+                    "checkItemsChecked": 0,
+                    "checkItemsEarliestDue": None,
+                    "comments": 0,
+                    "attachments": 0,
+                    "description": True,
+                    "due": None,
+                    "dueComplete": False,
+                    "start": None
+                },
+                "dueComplete": False,
+                "due": None,
+                "idChecklists": [],
+                "idMembers": [],
+                "labels": [],
+                "shortUrl": "https://trello.com/c/JlDCgB0e",
+                "start": None,
+                "subscribed": False,
+                "url": "https://trello.com/c/JlDCgB0e/22-coffee",
+                "cover": {
+                    "idAttachment": None,
+                    "color": None,
+                    "idUploadedBackground": None,
+                    "size": "normal",
+                    "brightness": "light",
+                    "idPlugin": None
+                }
+            },
+            {
+                "id": "6037ac8cd9b8ac5fba6946d7",
+                "checkItemStates": None,
+                "closed": False,
+                "dateLastActivity": "2021-03-02T16:08:46.455Z",
+                "desc": "Time to eat some cake because why the hell not!",
+                "descData": None,
+                "dueReminder": None,
+                "idBoard": "abcd1234",
+                "idList": "600ede7caeb0c67b818ec913",
+                "idMembersVoted": [],
+                "idShort": 30,
+                "idAttachmentCover": None,
+                "idLabels": [],
+                "manualCoverAttachment": False,
+                "name": "Cake time",
+                "pos": 65536,
+                "shortLink": "IUKu6VLB",
+                "isTemplate": False,
+                "cardRole": None,
+                "badges": {
+                    "attachmentsByType": {
+                        "trello": {
+                            "board": 0,
+                            "card": 0
+                        }
+                    },
+                    "location": False,
+                    "votes": 0,
+                    "viewingMemberVoted": False,
+                    "subscribed": False,
+                    "fogbugz": "",
+                    "checkItems": 0,
+                    "checkItemsChecked": 0,
+                    "checkItemsEarliestDue": None,
+                    "comments": 0,
+                    "attachments": 0,
+                    "description": True,
+                    "due": "2021-02-28T00:00:00.000Z",
+                    "dueComplete": False,
+                    "start": None
+                },
+                "dueComplete": False,
+                "due": "2021-02-28T00:00:00.000Z",
+                "idChecklists": [],
+                "idMembers": [],
+                "labels": [],
+                "shortUrl": "https://trello.com/c/IUKu6VLB",
+                "start": None,
+                "subscribed": False,
+                "url": "https://trello.com/c/IUKu6VLB/30-cake-time",
+                "cover": {
+                    "idAttachment": None,
+                    "color": None,
+                    "idUploadedBackground": None,
+                    "size": "normal",
+                    "brightness": "light",
+                    "idPlugin": None
+                }
+            },
+            {
+                "id": "603e2d668ecdb44ae6add243",
+                "checkItemStates": None,
+                "closed": False,
+                "dateLastActivity": "2021-03-02T12:19:51.004Z",
+                "desc": "",
+                "descData": None,
+                "dueReminder": None,
+                "idBoard": "abcd1234",
+                "idList": "600ede7caeb0c67b818ec913",
+                "idMembersVoted": [],
+                "idShort": 31,
+                "idAttachmentCover": None,
+                "idLabels": [],
+                "manualCoverAttachment": False,
+                "name": "moo",
+                "pos": 81920,
+                "shortLink": "TvCH1dTC",
+                "isTemplate": False,
+                "cardRole": None,
+                "badges": {
+                    "attachmentsByType": {
+                        "trello": {
+                            "board": 0,
+                            "card": 0
+                        }
+                    },
+                    "location": False,
+                    "votes": 0,
+                    "viewingMemberVoted": False,
+                    "subscribed": False,
+                    "fogbugz": "",
+                    "checkItems": 0,
+                    "checkItemsChecked": 0,
+                    "checkItemsEarliestDue": None,
+                    "comments": 0,
+                    "attachments": 0,
+                    "description": False,
+                    "due": "2021-04-01T00:00:00.000Z",
+                    "dueComplete": False,
+                    "start": None
+                },
+                "dueComplete": False,
+                "due": "2021-04-01T00:00:00.000Z",
+                "idChecklists": [],
+                "idMembers": [],
+                "labels": [],
+                "shortUrl": "https://trello.com/c/TvCH1dTC",
+                "start": None,
+                "subscribed": False,
+                "url": "https://trello.com/c/TvCH1dTC/31-moo",
+                "cover": {
+                    "idAttachment": None,
+                    "color": None,
+                    "idUploadedBackground": None,
+                    "size": "normal",
+                    "brightness": "light",
+                    "idPlugin": None
+                }
+            }
+        ]
+        response.json.return_value = json_return
         return response
     return None
