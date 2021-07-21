40f464e069b1cfa11bffb34accb05a3c80a9618a
Lachlan Pearse <lachlan.pearse@mclaren.com>
2021-07-21 13:23:09 +0100

    Tweaking startup script

--- start_todo-app.sh
+++ start_todo-app.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-poetry run gunicorn --error-logfile /app/gunicorn_error.log --access-logfile /app/gunicorn_access.log --bind 0.0.0.0:$PORT "todo_app.app:create_app()"
+$(poetry env info --path)/bin/gunicorn --error-logfile /app/gunicorn_error.log --access-logfile /app/gunicorn_access.log --bind 0.0.0.0:$PORT "todo_app.app:create_app()"
