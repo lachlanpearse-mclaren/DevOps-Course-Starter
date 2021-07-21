ff3b0ebfa3904f8227fb4bec4f61943537b95736
Lachlan Pearse <lachlan.pearse@mclaren.com>
2021-07-21 13:33:09 +0100

    More startup script trouble

--- start_todo-app.sh
+++ start_todo-app.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-$(poetry env info --path)/bin/gunicorn --error-logfile /app/gunicorn_error.log --access-logfile /app/gunicorn_access.log --bind 0.0.0.0:$PORT "todo_app.app:create_app()"
+export PATH=$PATH:$(poetry env info --path)/bin
+gunicorn --error-logfile /app/gunicorn_error.log --access-logfile /app/gunicorn_access.log --bind 0.0.0.0:$PORT "todo_app.app:create_app()"
