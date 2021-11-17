1ce3ee5b12246a10c25c6263295e357c660b286a
Lachlan Pearse <lachlan.pearse@mclaren.com>
2021-11-17 16:14:30 +0000

    Getting hello world to work

--- main.tf
+++ main.tf
@@ -1,14 +1,40 @@
 terraform {
-required_providers {
-azurerm = {
-source = "hashicorp/azurerm"
-version = ">= 2.49"
-}
-}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.49"
+    }
+  }
 }
 provider "azurerm" {
-features {}
+  features {}
 }
 data "azurerm_resource_group" "main" {
-name = "McLaren1_LachlanPearse_ProjectExercise"
+  name = "McLaren1_LachlanPearse_ProjectExercise"
+}
+
+resource "azurerm_app_service_plan" "main" {
+  name                = "lp-unit12-project-asp"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  kind                = "Linux"
+  reserved            = true
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "main" {
+  name                = "lp-unit12-projectexercise-hellowworld"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  app_service_plan_id = azurerm_app_service_plan.main.id
+  site_config {
+    app_command_line = ""
+    linux_fx_version = "DOCKER|nginxdemos/hello:latest"
+  }
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
+  }
 }
