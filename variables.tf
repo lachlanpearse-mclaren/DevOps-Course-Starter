1998bdcbb8560ee5520ec16389de3de7c5d4114c
Lachlan Pearse <lachlan.pearse@mclaren.com>
2021-11-18 15:32:11 +0000

    Remove some TF_VARs that are no longer needed

--- variables.tf
+++ variables.tf
@@ -8,32 +8,12 @@ variable "location" {
 variable "ARM_ACCESS_KEY" {
     description = "The private key for the tfstate storage container"
 }
-variable "FLASK_APP" {
-  description = "App Location"
-  sensitive   = true
-}
-variable "LOGIN_DISABLED" {
-  description = "Default LOGIN_DISABLED value"
-  sensitive   = true
-}
 variable "AUTH_CLIENTID" {
   description = "OAuth ClientID"
   sensitive   = true
 }
 variable "AUTH_SECRET" {
   description = "OAuth Secret"
-  sensitive   = true
-}
-variable "AUTH_REDIRECT_URL" {
-  description = "OAuth Redirect URL"
-  sensitive   = true
-}
-variable "AUTH_TOKEN_URL" {
-  description = "OAuth Token URL"
-  sensitive   = true
-}
-variable "AUTH_API_URL" {
-  description = "OAuth API URL"
   sensitive   = true
 }
 variable "APP_SECRET" {
