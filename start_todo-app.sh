9dbb723d3b1579c92bafaeaa62b262b943883bd7
Lachlan Pearse <lachlan.pearse@mclaren.com>
2021-11-18 11:25:31 +0000

    Remove explicit gunicorn logfile locations

--- start_todo-app.sh
+++ start_todo-app.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gunicorn --error-logfile /var/log/gunicorn_error.log --access-logfile /var/log/gunicorn_access.log --bind 0.0.0.0:$PORT "todo_app.app:create_app()"
+gunicorn --bind 0.0.0.0:$PORT "todo_app.app:create_app()"
