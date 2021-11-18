e94243660226f0e8a47ace4c9c43d1be8392e85b
Lachlan Pearse <lachlan.pearse@mclaren.com>
2021-11-18 09:01:29 +0000

    Configure env vars

--- main.tf
+++ main.tf
@@ -13,6 +13,19 @@ data "azurerm_resource_group" "main" {
   name = "McLaren1_LachlanPearse_ProjectExercise"
 }
 
+data "azurerm_cosmosdb_account" "main" {
+  name                = "lp-projectexercise-db"
+  resource_group_name = data.azurerm_resource_group.main.name
+  capabilities { name = "EnableServerless" }
+}
+
+data "azurerm_cosmosdb_mongo_database" "main" {
+  name                = "todo_app"
+  resource_group_name = data.azurerm_resource_group.main.name
+  account_name        = data.azurerm_cosmosdb_account.main.name
+
+}
+
 resource "azurerm_app_service_plan" "main" {
   name                = "lp-unit12-project-asp"
   location            = data.azurerm_resource_group.main.location
@@ -26,15 +39,26 @@ resource "azurerm_app_service_plan" "main" {
 }
 
 resource "azurerm_app_service" "main" {
-  name                = "lp-unit12-projectexercise-hellowworld"
+  name                = "lp-unit12-projectexercise-todo"
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
   app_service_plan_id = azurerm_app_service_plan.main.id
   site_config {
     app_command_line = ""
-    linux_fx_version = "DOCKER|nginxdemos/hello:latest"
+    linux_fx_version = "DOCKER|lachiexyz/todo-app:latest"
   }
   app_settings = {
     "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
+    "FLASK_APP"           = var.FLASK_APP
+    "MONGO_DB_CONNECTION" = var.MONGO_DB_CONNECTION
+    "MONGO_DB_NAME"       = var.MONGO_DB_NAME
+    "LOGIN_DISABLED"      = var.LOGIN_DISABLED
+    "AUTH_CLIENTID"       = var.AUTH_CLIENTID
+    "AUTH_SECRET"         = var.AUTH_SECRET
+    "AUTH_REDIRECT_URL"   = var.AUTH_REDIRECT_URL
+    "AUTH_TOKEN_URL"      = var.AUTH_TOKEN_URL
+    "AUTH_API_URL"        = var.AUTH_API_URL
+    "APP_SECRET"          = var.APP_SECRET  
   }
 }
+
---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "FLASK_APP" {
+  description = "App Location"
+  sensitive   = true
+}        
+variable "MONGO_DB_CONNECTION" {
+  description = "Mongo DB connection string"
+  sensitive   = true
+}
+variable "MONGO_DB_NAME" {
+  description = "Mongo DB name"
+  sensitive   = true
+}      
+variable "LOGIN_DISABLED" {
+  description = "Default LOGIN_DISABLED value"
+  sensitive   = true
+}   
+variable "AUTH_CLIENTID" {
+  description = "OAuth ClientID"
+  sensitive   = true
+}
+variable "AUTH_SECRET" {
+  description = "OAuth Secret"
+  sensitive   = true
+}    
+variable "AUTH_REDIRECT_URL" {
+  description = "OAuth Redirect URL"
+  sensitive   = true
+}
+variable "AUTH_TOKEN_URL" {
+  description = "OAuth Token URL"
+  sensitive   = true
+}
+variable "AUTH_API_URL" {
+  description = "OAuth API URL"
+  sensitive   = true
+}
+variable "APP_SECRET" {
+  description = "Application Secret"
+  sensitive   = true
+}
