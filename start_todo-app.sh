a6b8c986086b5ec90feec25a91101341d0a8a47f
Lachlan Pearse <lachlan.pearse@mclaren.com>
2021-11-18 11:15:09 +0000

    Startup script tweak to try different log location

--- start_todo-app.sh
+++ start_todo-app.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gunicorn --error-logfile /home/LogFiles/gunicorn_error.log --access-logfile /home/LogFiles/gunicorn_access.log --bind 0.0.0.0:$PORT "todo_app.app:create_app()"
+gunicorn --error-logfile /var/log/gunicorn_error.log --access-logfile /var/log/gunicorn_access.log --bind 0.0.0.0:$PORT "todo_app.app:create_app()"
